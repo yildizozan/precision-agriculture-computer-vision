7a933f971078f38060e21b40a04787db6ccf448f
Ozan Yıldız <yildizozan@users.noreply.github.com>
2020-04-01 11:50:46 +0300

    Update download-dataset.sh

--- download-dataset.sh
+++ download-dataset.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-#wget -O CA17.zip http://lcas.lincoln.ac.uk/nextcloud/index.php/s/RYni5xngnEZEFkR/download
-#unzip CA17.zip
-#mv CA17/carrots_labelled/ dataset/
-#rm -rf CA17
+rm -rf dataset data CA17
 
-rm -rf data
+wget -O CA17.zip http://lcas.lincoln.ac.uk/nextcloud/index.php/s/RYni5xngnEZEFkR/download
+unzip CA17.zip
+mv CA17/carrots_labelled/ dataset/
+rm -rf CA17
+
 mkdir -p data/train/img data/trainannot/img data/test/img data/testannot/img data/val/img data/valannot/img
 
 # Train & Test
@@ -34,4 +35,3 @@ do
         cp "dataset/$i/rgbreg_crop.png" "data/val/img/$i-rgbreg_crop.png";
         cp "dataset/$i/truth_crop.png" "data/valannot/img/$i-truth_crop.png";
 done
-
